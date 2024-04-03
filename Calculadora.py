9572a6f10d1ab19aac07975cc100b72036e8068e
LeonardoGodoiSiemens <164267592+LeonardoGodoiSiemens@users.noreply.github.com>
2024-04-03 10:16:24 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,18 +1,34 @@
-#Calculadora Parte 1
-print("Menu da Calculadora\n 1 - Somar\n 2 - Subtrair\n 3 - Multiplicar\n 4 - Dividir")
-opcao=int(input("Digite sua opção: "))
-numero1=float(input("Digite seu primeiro número: "))
-numero2=float(input("Digite seu segundo número: "))
+#Calculadora Parte 1 - Tópicos
+terminar=5
+sair=0
 soma="1"
 sub="2"
 multi="3"
 dividi="4"
-#Código da soma
-if opcao==1:
-    print(f"{numero1} + {numero2} = {numero1 + numero2}")
-elif opcao==2:
-    print(f"{numero1} - {numero2} = {numero1 - numero2}")
-elif opcao==3:
-    print(f"{numero1} * {numero2} = {numero1 * numero2}")
-elif opcao==4:
-    print(f"{numero1} / {numero2} = {numero1 / numero2}")
+#While 1 - Encerando em caso 0 ou caso não esteja entre as opções
+while terminar==5:
+    print("----| Menu da Calculadora |----\n 1 - Somar\n 2 - Subtrair\n 3 - Multiplicar\n 4 - Dividir\n 0 - Sair")
+    opcao=int(input("Digite sua opção: "))
+    if opcao==0:
+        print("Encerrando a calculadora...")
+        terminar=terminar+1
+    elif opcao<0 or opcao>4:
+        print(f"Essa opção {opcao} não existe")
+        terminar=terminar+1
+    elif opcao>0:
+        numero1=float(input("Digite seu primeiro número: "))
+        numero2=float(input("Digite seu segundo número: "))
+#While 2 - Resolvendo as contas e resetando a calculadora
+        while opcao>0:
+            if opcao==1:
+                print(f"{numero1} + {numero2} = {numero1 + numero2}")
+                opcao=opcao-1000
+            elif opcao==2:
+                print(f"{numero1} - {numero2} = {numero1 - numero2}")
+                opcao=opcao-1000
+            elif opcao==3:
+                print(f"{numero1} * {numero2} = {numero1 * numero2}")
+                opcao=opcao-1000
+            elif opcao==4:
+                print(f"{numero1} / {numero2} = {numero1 / numero2}")
+                opcao=opcao-1000
